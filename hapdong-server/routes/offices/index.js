800a479cc4101dceb6fe4bf10c75b95d548c427c
양정훈 <luciferkala@gmail.com>
2019-11-23 16:29:10 +0900

    add dummy data

--- hapdong-server/routes/offices/index.js
+++ hapdong-server/routes/offices/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.send("This is offices");
+  res.json({
+    success: "true",
+    message: "전체 조회 성공"
+  });
 });
 
 module.exports = router;
--- hapdong-server/routes/users/index.js
+++ hapdong-server/routes/users/index.js
@@ -2,8 +2,29 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("This is users");
+router.get("/signin", function(req, res, next) {
+  res.json({
+    status: 200,
+    success: true,
+    message: "로그인 성공"
+  });
 });
 
+router.post("/signup", function(req, res, next) {
+  res.json({
+    status: 200,
+    success: true,
+    message: "회원가입 성공"
+  });
+});
+
+router.get("/:userIdx", function(req, res, next) {
+  const userIdx = req.params.userIdx;
+  console.log(userIdx);
+  res.json({
+    status: 200,
+    success: true,
+    message: "로그인 성공"
+  });
+});
 module.exports = router;
