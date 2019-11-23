e5ff1dc68b46895f48e6ed56e3139c28e90304b0
양정훈 <luciferkala@gmail.com>
2019-11-23 16:20:39 +0900

    basic route

--- hapdong-server/routes/index.js
+++ hapdong-server/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userRouter = require("./users");
+const officeRouter = require("./offices");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+router.use("/users", userRouter);
+router.use("/offices", officeRouter);
 module.exports = router;
