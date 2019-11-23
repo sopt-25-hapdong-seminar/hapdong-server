df6fa22025285728916f7d175f2429b1fe681c97
양정훈 <luciferkala@gmail.com>
2019-11-23 17:05:52 +0900

    signin

--- hapdong-server/routes/users/index.js
+++ hapdong-server/routes/users/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/signin", function(req, res, next) {
+router.post("/signin", function(req, res, next) {
   res.json({
     status: 200,
     success: true,
