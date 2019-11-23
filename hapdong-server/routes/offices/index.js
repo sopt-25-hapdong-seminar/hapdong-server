a4fb82ea0b63ff4eebcc6254ba2f326e4932e1bb
양정훈 <luciferkala@gmail.com>
2019-11-23 17:03:58 +0900

    with data

--- hapdong-server/routes/offices/index.js
+++ hapdong-server/routes/offices/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
+/* GET home page. */ i;
 router.get("/", function(req, res, next) {
   res.json({
     success: "true",
-    message: "전체 조회 성공"
+    message: "전체 조회 성공",
+    data: [
+      { officeIdx: 1, officeNum: 100, numOfBicycle: 7 },
+      { officeIdx: 2, officeNum: 101, numOfBicycle: 9 },
+      { officeIdx: 3, officeNum: 102, numOfBicycle: 2 }
+    ]
   });
 });
 
