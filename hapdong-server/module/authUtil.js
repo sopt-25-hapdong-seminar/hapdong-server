34128f4d46c21757c98364ca55904e907e9b4d19
artiiicy <mjun09@naver.com>
2019-11-23 16:22:52 +0900

    modify utils

---
+++ hapdong-server/module/authUtil.js
@@ -0,0 +1,18 @@
+// authUtil.js
+const authUtil = {
+    successTrue: (message, data) => {
+        return {
+            success: true,
+            message: message,
+            data: data
+        }
+    },
+    successFalse: (message) => {
+        return {
+            success: false,
+            message: message
+        }
+    },
+}
+
+module.exports = authUtil
---
+++ hapdong-server/module/responseMessage.js
@@ -0,0 +1,14 @@
+// responseMessage.js
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다.",
+    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+    USER_READ_SUCCESS: "유저 조회 성공.",
+    USER_READ_FAIL: "유저 조회 성공.",
+    OFFICE_READ_SUCCESS: "대여소 조회 성공.",
+    OFFICE_READ_FAIL: "대여소 조회 성공.",
+    NO_OFFICE : "존재하지 않는 대여소입니다.",
+    NO_USER: "존재하지 않는 유저 입니다.",
+    ALREADY_OFFICE : "존재하는 대여소입니다.",
+    ALREADY_USER: "존재하는 유저 입니다.",
+    INTERNAL_SERVER_ERROR: "서버 내부 오류."
+}
